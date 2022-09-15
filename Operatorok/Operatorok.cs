f13072198b4ad241433ea50d2c3733c5bc102ffe
ADRIAN30 <medveadrian.szgya@gmail.com>
2022-09-15 12:01:30 +0200

    Operatorok class létrehozva és 3. feladatig kész

---
+++ Operatorok/Operatorok.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorok
+{
+    class Operatorok
+    {
+        private int elsoOperandus;
+
+        public int ElsoOperandus
+        {
+            get { return elsoOperandus; }
+            private set { elsoOperandus = value; }
+        }
+
+        private string karakter;
+
+        public string Karakter
+        {
+            get { return karakter; }
+            private set { karakter = value; }
+        }
+
+        private int masodikOperandus;
+
+        public int MasodikOperandus
+        {
+            get { return masodikOperandus; }
+            private set { masodikOperandus = value; }
+        }
+
+        public Operatorok(int elsoOperandus, string karakter, int masodikOperandus)
+        {
+            this.elsoOperandus = elsoOperandus;
+            this.karakter = karakter;
+            this.masodikOperandus = masodikOperandus;
+        }
+    }
+}
--- Operatorok/Program.cs
+++ Operatorok/Program.cs
@@ -3,14 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Operatorok
 {
     class Program
     {
+        static List<Operatorok> operatorok = new List<Operatorok>();
         static void Main(string[] args)
         {
+            ElsoFeladat();
+            MasodikFeladat();
+            HarmadikFeladat();
 
+            Console.ReadKey();
+        }
+
+        private static void HarmadikFeladat()
+        {
+            Console.WriteLine("3. feladat: Kifejezések maradékos osztással: " + operatorok.Count(i => i.Karakter == "mod"));
+        }
+
+        private static void MasodikFeladat()
+        {
+            Console.WriteLine("2. feladat: Kifejezések száma: " + operatorok.Count);
+        }
+
+        private static void ElsoFeladat()
+        {
+            FajlBeolvasas();
+        }
+
+        private static void FajlBeolvasas()
+        {
+            using (StreamReader sr = new StreamReader("kifejezesek.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] sor = sr.ReadLine().Split(' ');
+
+                    operatorok.Add(new Operatorok(Convert.ToInt32(sor[0]), sor[1], Convert.ToInt32(sor[2])));
+                }
+            }
         }
     }
 }
